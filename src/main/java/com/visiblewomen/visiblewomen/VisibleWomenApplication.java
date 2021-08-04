44184d2f03760f8974d53d667328ef1643469a33
tassenabi <abi.tassen@posteo.de>
2021-08-04 13:43:58 +0200

    init project

---
+++ src/main/java/com/visiblewomen/visiblewomen/VisibleWomenApplication.java
@@ -0,0 +1,13 @@
+package com.visiblewomen.visiblewomen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibleWomenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisibleWomenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visiblewomen/visiblewomen/VisibleWomenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visiblewomen.visiblewomen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisibleWomenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
