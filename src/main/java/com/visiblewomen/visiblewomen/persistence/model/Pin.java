97d80f7f068971741a4aa127eb5fd0927ad66972
tassenabi <abi.tassen@posteo.de>
2021-08-05 17:39:50 +0200

    Basti mag Yvonne und hat sich auch gefickt

--- src/main/java/com/visiblewomen/visiblewomen/persistence/model/Pin.java
+++ src/main/java/com/visiblewomen/visiblewomen/persistence/model/Pin.java
@@ -3,17 +3,23 @@ package com.visiblewomen.visiblewomen.persistence.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.joda.time.DateTime;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
 
 @Getter
 @Setter
+@Document(collection = "pin")
 public class Pin {
 
+    @Id
     private String id;
     private String whatHappenedText;
     private GeoJsonPoint geoJsonPoint;
     private DateTime timeHappened;
-    private HarrasCategory[] whatHappenedCategories;
+    private ArrayList<HarrasCategory> whatHappenedCategories;
 
     @Getter
     public enum HarrasCategory {
