07258f3636d0978bd17fd5896b344cbeccb28213
tassenabi <abi.tassen@posteo.de>
2021-08-23 16:35:54 +0200

    ...

--- src/main/java/com/visiblewomen/visiblewomen/web/Controller.java
+++ src/main/java/com/visiblewomen/visiblewomen/web/Controller.java
@@ -3,6 +3,7 @@ package com.visiblewomen.visiblewomen.web;
 import com.visiblewomen.visiblewomen.persistence.model.Pin;
 import com.visiblewomen.visiblewomen.persistence.service.PinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,8 +25,13 @@ public class Controller {
 
     //ResponseEntity fehlt hier noch
     @GetMapping("/pin/{id}")
-    public Optional<Pin> getPin(@PathVariable String id){
-        return this.pinService.getPin(id);
+    public ResponseEntity<Pin> getPin(@PathVariable String id){
+        Optional<Pin> response = pinService.getPin(id);
+
+        return response != null ?
+                ResponseEntity.ok(response) :
+                ResponseEntity.noContent().build();
+
     }
 
     @PostMapping("/pin/{id}")
